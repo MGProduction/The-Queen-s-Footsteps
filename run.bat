a6b2ca0cffe8a7adb1f1aae61ecdb5c8f9536021
Marco Giorgini <marco.giorgini@gmail.com>
2024-02-17 13:42:54 +0100

    initial update

---
+++ run.bat
@@ -0,0 +1,11 @@
+del *.h
+del advcartridge
+del make.bat
+builder\script_compiler.exe game.hjt
+if %ERRORLEVEL% EQU 0 (
+	pause
+) else (
+	pause
+	call make builder/storytllr64.prg
+	bin\queensfootsteps.d64
+)
